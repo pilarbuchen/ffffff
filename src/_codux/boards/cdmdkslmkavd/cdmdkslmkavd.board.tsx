54454b1233d6ccdb01e3cd12363c42d1bdba8c6e
pilarbuchen <pilarb@wix.com>
2024-06-03 13:32:26 -0500

    commit branch

---
+++ src/_codux/boards/cdmdkslmkavd/cdmdkslmkavd.board.tsx
@@ -0,0 +1,8 @@
+import { createBoard } from '@wixc3/react-board';
+import { Cdmdkslmkavd } from '../../../components/cdmdkslmkavd/cdmdkslmkavd';
+
+export default createBoard({
+    name: 'Cdmdkslmkavd',
+    Board: () => <Cdmdkslmkavd />,
+    isSnippet: true,
+});
