185a7b0d831272eb375019983073730d88f322fc
pilarbuchen <pilarb@wix.com>
2024-06-03 13:48:12 -0500

    nkjkbj

--- src/_codux/boards/nlkklnlk/nlkklnlk.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Nlkklnlk } from '../../../components/nlkklnlk/nlkklnlk';
-
-export default createBoard({
-    name: 'Nlkklnlk',
-    Board: () => <Nlkklnlk />,
-    isSnippet: true,
-});
